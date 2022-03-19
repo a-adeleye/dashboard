f4bf5afa3fc3653e6caede446ccdc06834ba33a8
aadeleye <adeleyeadeyemib@gmail.com>
2022-03-19 23:02:10 +0100

    avatar updated

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,28 +1,25 @@
-import avi from "../avi.png";
+import Avatar from "./Avatar";
+import { Link } from "react-router-dom";
+import ProfileName from "./ProfileName";
 
 export default function HomePage() {
   return (
     <>
-      <div className=" flex items-center justify-center py-12 px-4 sm:px-6 bg-white lg:px-8 w-2/5 min-w-fit max-h-fit rounded-xl">
+      <div className=" flex items-center justify-center py-12 px-4 px-6 bg-white lg:px-8 w-4/5 min-w-max max-w-sm max-h-fit rounded-xl">
         <div className="max-w-md w-full space-y-8">
-          <div>
-            <img
-              className="mx-auto h-12 w-20 h-auto rounded-full"
-              src={avi}
-              alt="Workflow"
-            />
-            <h2 className="mt-6 text-center text-xl font-extrabold text-gray-900">
-              Welcome username
-            </h2>
-          </div>
-
-          <button
-            type="submit"
-            className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-black bg-yellow-300 hover:bg-yellow-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-500"
-          >
-            <span className="absolute left-0 inset-y-0 flex items-center pl-3"></span>
-            Edit Profile
-          </button>
+          <Avatar />
+          <h2 className="mt-6 text-center text-l font-bold text-gray-900">
+            Welcome <ProfileName />
+          </h2>
+          <Link to="profile">
+            <button
+              type="submit"
+              className="group relative w-full flex justify-center mt-4 py-2 px-4 border border-transparent text-sm font-bold rounded-md text-black bg-yellow-300 hover:bg-yellow-500 focus:outline-none"
+            >
+              <span className="absolute left-0 inset-y-0 flex items-center pl-3"></span>
+              Edit Profile
+            </button>
+          </Link>
         </div>
       </div>
     </>
