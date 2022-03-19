9f1bd42bf4e3e90d7ac627c5f37216ebff538ca2
aadeleye <adeleyeadeyemib@gmail.com>
2022-03-19 23:01:40 +0100

    links updated

--- src/components/Login.js
+++ src/components/Login.js
@@ -76,14 +76,14 @@ export default function LoginPage({handleClick}) {
             </div>
 
             <div>
-              <button
+              <Link to="dashboard"><button
                 onClick={handleClick}
                 type="submit"
-                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-black bg-yellow-300 hover:bg-yellow-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-500"
+                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-bold rounded-md text-black bg-yellow-300 hover:bg-yellow-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-500"
               >
                 <span className="absolute left-0 inset-y-0 flex items-center pl-3"></span>
                 Sign in
-              </button>
+              </button></Link>
             </div>
           </form>
         </div>
