cc889e0fb362093697317ed51bc935fbd8a2ac72
aadeleye <adeleyeadeyemib@gmail.com>
2022-03-20 13:19:34 +0100

    logo component separated

---
+++ src/components/FlickLogo.js
@@ -0,0 +1,12 @@
+export default function FlickLogo() {
+    return (
+      <div>
+        <img
+          className="mx-auto h-12 w-auto"
+          src="https://flick.ae/wp-content/themes/flick/img/FlickLogo.png"
+          alt="Workflow"
+        />
+        
+      </div>
+    );
+  }
