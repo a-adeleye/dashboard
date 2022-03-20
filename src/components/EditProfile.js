1d9f0b38d4860422434c7aefd338acf23dcb10e1
aadeleye <adeleyeadeyemib@gmail.com>
2022-03-20 01:35:39 +0100

    modal updated

--- src/components/EditProfile.js
+++ src/components/EditProfile.js
@@ -40,7 +40,7 @@ export default function EditProfile(props) {
               leaveFrom="opacity-100 scale-100"
               leaveTo="opacity-0 scale-95"
             >
-              <div className="inline-block w-5/12 h-48 border-2 border-black max-w-md p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-2xl">
+              <div className="inline-block w-5/12 h-48 border-2 border-black max-w-md min-w-max p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-2xl">
                 <Dialog.Title
                   as="h3"
                   className="text-lg font-medium leading-6 text-gray-900"
