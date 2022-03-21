ee836951c214adbe5b239674f581d0016c8c5b58
aadeleye <adeleyeadeyemib@gmail.com>
2022-03-21 17:43:28 +0100

    login form updated

--- src/components/Login.js
+++ src/components/Login.js
@@ -41,7 +41,7 @@ export default function LoginPage() {
         <div className="w-full max-w-md space-y-8">
           <FlickLogo />
           <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-            Log in
+            Welcome
           </h2>
           <div className="flex items-center justify-center">
             <Loading />
